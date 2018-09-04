a8f89587b63b7c825c4cb9a541db6d56a343cd72
babuino933 <42853001+babuino933@users.noreply.github.com>
2018-09-03 21:16:41 -0300

    Add files via upload

--- bd_jogo.sql
+++ bd_jogo.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 30-Ago-2018 às 03:19
+-- Tempo de geração: 04/09/2018 às 02:16
 -- Versão do servidor: 5.7.11-log
--- PHP Version: 5.6.15
+-- Versão do PHP: 5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `bd_jogo`
+-- Banco de dados: `bd_jogo`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `jogadores`
+-- Estrutura para tabela `jogadores`
 --
 
 CREATE TABLE `jogadores` (
@@ -39,7 +39,7 @@ CREATE TABLE `jogadores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tabela dos jogadores';
 
 --
--- Extraindo dados da tabela `jogadores`
+-- Fazendo dump de dados para tabela `jogadores`
 --
 
 INSERT INTO `jogadores` (`id`, `nome`, `sobrenome`, `usuario`, `avatar`, `data_cadastro`, `email`, `senha`, `data_nasci`) VALUES
@@ -49,7 +49,7 @@ INSERT INTO `jogadores` (`id`, `nome`, `sobrenome`, `usuario`, `avatar`, `data_c
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `jogadores_has_questoes`
+-- Estrutura para tabela `jogadores_has_questoes`
 --
 
 CREATE TABLE `jogadores_has_questoes` (
@@ -61,7 +61,7 @@ CREATE TABLE `jogadores_has_questoes` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `logs`
+-- Estrutura para tabela `logs`
 --
 
 CREATE TABLE `logs` (
@@ -73,7 +73,7 @@ CREATE TABLE `logs` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `niveis`
+-- Estrutura para tabela `niveis`
 --
 
 CREATE TABLE `niveis` (
@@ -82,7 +82,7 @@ CREATE TABLE `niveis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tabela de níveis';
 
 --
--- Extraindo dados da tabela `niveis`
+-- Fazendo dump de dados para tabela `niveis`
 --
 
 INSERT INTO `niveis` (`id`, `nome`) VALUES
@@ -91,7 +91,7 @@ INSERT INTO `niveis` (`id`, `nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `professores`
+-- Estrutura para tabela `professores`
 --
 
 CREATE TABLE `professores` (
@@ -107,7 +107,7 @@ CREATE TABLE `professores` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `provas`
+-- Estrutura para tabela `provas`
 --
 
 CREATE TABLE `provas` (
@@ -118,32 +118,34 @@ CREATE TABLE `provas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tabela de provas';
 
 --
--- Extraindo dados da tabela `provas`
+-- Fazendo dump de dados para tabela `provas`
 --
 
 INSERT INTO `provas` (`id`, `temas_id`, `niveis_id`, `nome`) VALUES
-(1, 1, 1, 'Introdução HTML');
+(1, 1, 1, 'Introdução HTML'),
+(2, 2, 1, 'Introdução PHP'),
+(3, 3, 1, 'Introdução CSS');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `questoes`
+-- Estrutura para tabela `questoes`
 --
 
 CREATE TABLE `questoes` (
   `enunciado` varchar(200) NOT NULL,
   `id` int(10) UNSIGNED NOT NULL COMMENT 'identificação dos níveis',
-  `alternativa_a` varchar(45) NOT NULL COMMENT 'questão A',
-  `alternativa_b` varchar(45) NOT NULL COMMENT 'questão B',
-  `alternativa_c` varchar(45) NOT NULL COMMENT 'questão C',
-  `alternativa_d` varchar(45) NOT NULL COMMENT 'questão D',
-  `correta` varchar(45) NOT NULL COMMENT 'atributo que guardará a questão correta',
+  `alternativa_a` varchar(200) NOT NULL COMMENT 'questão A',
+  `alternativa_b` varchar(200) NOT NULL COMMENT 'questão B',
+  `alternativa_c` varchar(200) NOT NULL COMMENT 'questão C',
+  `alternativa_d` varchar(200) NOT NULL COMMENT 'questão D',
+  `correta` varchar(200) NOT NULL COMMENT 'atributo que guardará a questão correta',
   `peso` int(11) NOT NULL COMMENT 'atributo onde vamos definir o valor que valerá as questões',
   `provas_id` int(10) UNSIGNED NOT NULL COMMENT 'atributo identificador da tabela provas'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tabela das questões';
 
 --
--- Extraindo dados da tabela `questoes`
+-- Fazendo dump de dados para tabela `questoes`
 --
 
 INSERT INTO `questoes` (`enunciado`, `id`, `alternativa_a`, `alternativa_b`, `alternativa_c`, `alternativa_d`, `correta`, `peso`, `provas_id`) VALUES
@@ -156,12 +158,32 @@ INSERT INTO `questoes` (`enunciado`, `id`, `alternativa_a`, `alternativa_b`, `al
 ('Qual caractere é usado para indicar uma tag final?', 7, '<', '*', '^', '/', 'D', 1, 1),
 ('Como você pode abrir um link em uma nova aba / janela do navegador?', 8, '<a href="url" new>', '<a href="url" target="_blank">', '<a href="url" target="new">', '<a href="url" new ="url">', 'B', 1, 1),
 ('Quais desses elementos são todos os elementos <table>?', 9, '<table><tr><td>', '<table><head><tfoot>', '<thead><body><tr>', '<table><tr><tt>', 'D', 1, 1),
-('Como você pode fazer uma lista numerada?', 10, '<ul>', '<list>', '<ol>', '<dl>', 'C', 1, 1);
+('Como você pode fazer uma lista numerada?', 10, '<ul>', '<list>', '<ol>', '<dl>', 'C', 1, 1),
+('O que significa o PHP?', 11, 'Hypertext Preprocessor', 'Personal Hypertext Processor', 'Private Home Page', 'Prototype hypertext page', 'A', 1, 2),
+('Quais os delimitadores que cercam os scripts do PHP?', 12, '<?php>...</?>', '<?php...?>', '<script>...</script>', '<&>...</&>', 'B', 1, 2),
+('Como você escreve "Hello World" em PHP?', 13, '"Hello World";', 'Document.Write("Hello World");', 'echo "Hello World";', '<"Hello World">;', 'C', 1, 2),
+('Todas as variáveis no PHP começam com qual símbolo?', 14, '&', '!', '$', '#', 'C', 1, 2),
+('Qual é a maneira correta de terminar uma instrução PHP?', 15, '.', 'New line', ';', '</php>', 'C', 1, 2),
+('Como você obtém informações de um formulário que é enviado usando o método "get"?', 16, 'Request.Form;', 'Request.QueryString;', '$_GET[];', '<$_GET>', 'C', 1, 2),
+('Qual é a maneira correta de se conectar a um banco de dados MySQL?', 17, 'mysql_select_db();', 'mysql_connect();', 'connect();', 'connect_db();', 'B', 1, 2),
+('Qual é a maneira correta de criar uma função em PHP?', 18, 'Function_Myfunction();', 'Myfunction()', 'Create_function();', 'Function Myfunction();', 'D', 1, 2),
+('A sintaxe do PHP é mais semelhante a:', 19, 'Java Script', 'C & perl', 'ASP', 'Asp.net', 'B', 1, 2),
+('Qual é a maneira correta de abrir o arquivo "time.txt" como legível?', 20, 'open("time.txt");', 'open_file(“time.txt”);', 'fopen(“time.txt”,”r”);', 'fopen(“time.txt”);', 'C', 1, 2),
+('O que significa o CSS?', 21, 'Creative Style Sheets', 'Cascading Style Sheets', 'Colorful Style Sheets', 'Computer Style Sheets', 'B', 1, 3),
+('Qual é o HTML correto para se referir a uma folha de estilos externa?', 22, '<stylesheet>mystyle.css</stylesheet>', '<link rel="stylesheet"type="text/css" href="mystyle.css">', '<style src="mystyle.css">', '<link src="mystyle.css">', 'B', 1, 3),
+('Qual tag HTML é usada para definir uma folha de estilo interna?', 23, '<css>', '<script>', '<style>', '<stylesheet>', 'C', 1, 3),
+('Qual atributo HTML é usado para definir estilos embutidos?', 24, 'font', 'styles', 'style', 'class', 'C', 1, 3),
+('Qual é a sintaxe CSS correta?', 25, 'body {color: black}', 'body:color=black;', '{body;color:black;}', '{body:color=black;}', 'A', 1, 3),
+('Como você insere um comentário em um arquivo CSS?', 26, '// this is a comment', '''this is a comment', '/*this is a comment*/', '//this is a comment//', 'C', 1, 3),
+('Qual propriedade é usada para alterar a cor do plano de fundo?', 27, 'color', 'background-color', 'bgcolor', 'background-collor', 'B', 1, 3),
+('Como você adiciona uma cor de fundo para todos os elementos <h1>?', 28, 'h1 {background-color:#FFFFFF;}', 'all.h1 {background-color:#FFFFFF;}', 'h1.all {background-color:#FFFFFF;}', 'h1({background-color:#FFFFFF;})', 'B', 1, 3),
+('Qual propriedade CSS é usada para alterar a cor do texto de um elemento?', 29, 'text-color', 'color', 'fgcolor', 'color_text', 'B', 1, 3),
+('Qual propriedade CSS controla o tamanho do texto?', 30, 'text-size', 'font-size', 'text-style', 'font-style', 'B', 1, 3);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `temas`
+-- Estrutura para tabela `temas`
 --
 
 CREATE TABLE `temas` (
@@ -170,24 +192,26 @@ CREATE TABLE `temas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='tabela onde conterá os temas das provas ';
 
 --
--- Extraindo dados da tabela `temas`
+-- Fazendo dump de dados para tabela `temas`
 --
 
 INSERT INTO `temas` (`id`, `nome`) VALUES
-(1, 'HTML');
+(1, 'HTML'),
+(2, 'PHP'),
+(3, 'CSS');
 
 --
--- Indexes for dumped tables
+-- Índices de tabelas apagadas
 --
 
 --
--- Indexes for table `jogadores`
+-- Índices de tabela `jogadores`
 --
 ALTER TABLE `jogadores`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `jogadores_has_questoes`
+-- Índices de tabela `jogadores_has_questoes`
 --
 ALTER TABLE `jogadores_has_questoes`
   ADD PRIMARY KEY (`jogadores_id`,`questoes_id`),
@@ -195,26 +219,26 @@ ALTER TABLE `jogadores_has_questoes`
   ADD KEY `fk_jogadores_has_questoes_jogadores_idx` (`jogadores_id`);
 
 --
--- Indexes for table `logs`
+-- Índices de tabela `logs`
 --
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_logs_jogadores1_idx` (`jogadores_id`);
 
 --
--- Indexes for table `niveis`
+-- Índices de tabela `niveis`
 --
 ALTER TABLE `niveis`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `professores`
+-- Índices de tabela `professores`
 --
 ALTER TABLE `professores`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `provas`
+-- Índices de tabela `provas`
 --
 ALTER TABLE `provas`
   ADD PRIMARY KEY (`id`),
@@ -222,78 +246,78 @@ ALTER TABLE `provas`
   ADD KEY `fk_provas_niveis1_idx` (`niveis_id`);
 
 --
--- Indexes for table `questoes`
+-- Índices de tabela `questoes`
 --
 ALTER TABLE `questoes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_questoes_provas1_idx` (`provas_id`);
 
 --
--- Indexes for table `temas`
+-- Índices de tabela `temas`
 --
 ALTER TABLE `temas`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas apagadas
 --
 
 --
--- AUTO_INCREMENT for table `jogadores`
+-- AUTO_INCREMENT de tabela `jogadores`
 --
 ALTER TABLE `jogadores`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'indetificação da tabela jogadores', AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `niveis`
+-- AUTO_INCREMENT de tabela `niveis`
 --
 ALTER TABLE `niveis`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação dos níveis', AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação dos níveis', AUTO_INCREMENT=3;
 --
--- AUTO_INCREMENT for table `professores`
+-- AUTO_INCREMENT de tabela `professores`
 --
 ALTER TABLE `professores`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação da tabela professores';
 --
--- AUTO_INCREMENT for table `provas`
+-- AUTO_INCREMENT de tabela `provas`
 --
 ALTER TABLE `provas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identicação das provas', AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identicação das provas', AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT for table `questoes`
+-- AUTO_INCREMENT de tabela `questoes`
 --
 ALTER TABLE `questoes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação dos níveis', AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação dos níveis', AUTO_INCREMENT=31;
 --
--- AUTO_INCREMENT for table `temas`
+-- AUTO_INCREMENT de tabela `temas`
 --
 ALTER TABLE `temas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação do tema', AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'identificação do tema', AUTO_INCREMENT=4;
 --
--- Constraints for dumped tables
+-- Restrições para dumps de tabelas
 --
 
 --
--- Limitadores para a tabela `jogadores_has_questoes`
+-- Restrições para tabelas `jogadores_has_questoes`
 --
 ALTER TABLE `jogadores_has_questoes`
   ADD CONSTRAINT `fk_jogadores_has_questoes_jogadores` FOREIGN KEY (`jogadores_id`) REFERENCES `jogadores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_jogadores_has_questoes_questoes1` FOREIGN KEY (`questoes_id`) REFERENCES `questoes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `logs`
+-- Restrições para tabelas `logs`
 --
 ALTER TABLE `logs`
   ADD CONSTRAINT `fk_logs_jogadores1` FOREIGN KEY (`jogadores_id`) REFERENCES `jogadores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `provas`
+-- Restrições para tabelas `provas`
 --
 ALTER TABLE `provas`
   ADD CONSTRAINT `fk_provas_niveis1` FOREIGN KEY (`niveis_id`) REFERENCES `niveis` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_provas_temas1` FOREIGN KEY (`temas_id`) REFERENCES `temas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Limitadores para a tabela `questoes`
+-- Restrições para tabelas `questoes`
 --
 ALTER TABLE `questoes`
   ADD CONSTRAINT `fk_questoes_provas1` FOREIGN KEY (`provas_id`) REFERENCES `provas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
